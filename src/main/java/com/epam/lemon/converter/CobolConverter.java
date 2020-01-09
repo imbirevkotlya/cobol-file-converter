2231669efb95d795f8fd1fa0fa7befb9d54f6ca3
Nikolai Imbirev <nikolai_imbirev@epam.com>
2020-01-09 16:56:13 +0300

    refactor the cobol converter logic using new statement value iterator logic with statement value encapsulation class

--- src/main/java/com/epam/lemon/converter/CobolConverter.java
+++ src/main/java/com/epam/lemon/converter/CobolConverter.java
@@ -2,20 +2,15 @@ package com.epam.lemon.converter;
 
 import com.epam.lemon.dataset.Dataset;
 import com.epam.lemon.exception.InvalidDataException;
-import com.epam.lemon.field.AlphanumericFieldConverter;
-import com.epam.lemon.field.FieldConverter;
-import com.epam.lemon.field.IntegerFieldConverter;
+import com.epam.lemon.field.*;
 import com.epam.lemon.record.Encoding;
 import com.epam.lemon.record.Record;
 import com.epam.lemon.record.RecordIterator;
-import com.epam.lemon.statement.DataDeclarationCobolStatement;
-import com.epam.lemon.statement.GroupDataDeclarationCobolStatement;
-import com.epam.lemon.statement.RegularDataDeclarationCobolStatement;
 import com.epam.lemon.statement.StatementType;
 
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class CobolConverter {
@@ -39,41 +34,59 @@ public class CobolConverter {
   }
 
   private Record convertRecord(Record record, Encoding sourceEncoding, Encoding targetEncoding) throws InvalidDataException {
-    byte[] recordValue = record.getValue();
-    Record resultRecord = new Record(record.getRecordStructure());
-    ByteBuffer buffer = ByteBuffer.allocate(recordValue.length);
-    int startPosition = 0;
-    for (DataDeclarationCobolStatement cobolStatement : record.getRecordStructure().getCobolStatements()) {
-      if (cobolStatement.getStatementType().equals(StatementType.GROUP_STATEMENT)) {
-        GroupDataDeclarationCobolStatement groupStatement = (GroupDataDeclarationCobolStatement) cobolStatement;
-        List<DataDeclarationCobolStatement> childrenStatements = groupStatement.getChildrenStatements();
-        for (DataDeclarationCobolStatement childStatement : childrenStatements) {
-          RegularDataDeclarationCobolStatement regularStatement = (RegularDataDeclarationCobolStatement) childStatement;
-          Integer fieldLength = regularStatement.getLength();
-          byte[] fieldValue = Arrays.copyOfRange(recordValue, startPosition, startPosition + fieldLength);
-          startPosition += fieldLength;
-          for (FieldConverter fieldConverter : fieldConverters) {
-            if (fieldConverter.getStatementType().equals(regularStatement.getStatementType())) {
-              byte[] resultValue = fieldConverter.convertValue(fieldValue, sourceEncoding, targetEncoding);
-              buffer.put(resultValue);
-            }
-          }
-        }
-      }
-      else {
-        RegularDataDeclarationCobolStatement regularStatement = (RegularDataDeclarationCobolStatement) cobolStatement;
-        Integer fieldLength = regularStatement.getLength();
-        byte[] fieldValue = Arrays.copyOfRange(recordValue, startPosition, startPosition + fieldLength);
-        startPosition += fieldLength;
-        for (FieldConverter fieldConverter : fieldConverters) {
-          if (fieldConverter.getStatementType().equals(regularStatement.getStatementType())) {
-            byte[] resultValue = fieldConverter.convertValue(fieldValue, sourceEncoding, targetEncoding);
-            buffer.put(resultValue);
-          }
+    StatementValueIterator statementValueIterator = new StatementValueIterator(record);
+    ByteBuffer buffer = ByteBuffer.allocate(statementValueIterator.getRecordLength());
+    while (statementValueIterator.hasNext()) {
+      for (FieldConverter fieldConverter : fieldConverters) {
+        StatementValue statementValue = statementValueIterator.next();
+        if (fieldConverter.getStatementType().equals(statementValue.statementType)) {
+          byte[] resultValue = fieldConverter.convertValue(statementValue.value, sourceEncoding, targetEncoding);
+          buffer.put(resultValue);
         }
       }
     }
+    Record resultRecord = new Record(record.getRecordStructure());
     resultRecord.setValue(buffer.array());
     return resultRecord;
   }
+
+  private static class StatementValueIterator implements Iterator<StatementValue> {
+
+    private final Record record;
+    private final Integer recordLength;
+
+    StatementValueIterator(Record record) {
+      this.record = record;
+      recordLength = null;
+    }
+
+    @Override
+    public boolean hasNext() {
+      return false;
+    }
+
+    @Override
+    public StatementValue next() {
+      return null;
+    }
+
+    Integer getRecordLength() {
+      return recordLength;
+    }
+  }
+
+  private static class StatementValue {
+
+    private final byte[] value;
+    private final StatementType statementType;
+    private final Integer length;
+
+    public StatementValue(byte[] value, StatementType statementType, Integer length) {
+      this.value = value;
+      this.statementType = statementType;
+      this.length = length;
+    }
+  }
+
 }
+
